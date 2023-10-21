76841c41a6362db13d15688a96bc7bb2e6a59ade
Hadestech01 <jaolaniyi96@student.lautech.edu.ng>
2023-10-21 01:45:25 +0100

    too

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+// import { SHOP_DATA } from "../utils/db";
 
 function Footer() {
   return (
     <footer className="w-full h-[5rem] bg-slate-400 flex flex-col justify-end text-center">
+      <div className="w-2/4 h-20 m-auto my-5">
+        {/* {SHOP_DATA[4].items.map((item) => (
+          <div key={item.id}>
+            <img src={item.imageUrl} alt={item.name} />
+          </div>
+        ))} */}
+      </div>
       <p className="pb-1">
         Designed By{" "}
         <a
